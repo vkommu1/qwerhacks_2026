1de1889ab3e99e1c37f178f836f334140f1112dd
Sophia Yun <sophia@sophias-mac.local>
2026-02-07 17:18:56 -0800

    sophia test

---
+++ client/src/components/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+
+	render() {
+		return(
+			<div>
+				floofy
+				hi!
+				lemonpoppy
+			</div>
+		);
+	}
+}
+
+export default Home;
